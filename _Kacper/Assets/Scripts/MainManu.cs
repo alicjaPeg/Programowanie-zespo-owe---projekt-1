a9004ca68805bf84c1a6d148286ae3d278e306f7
Kacper Gorgoń <61627168+Kugger@users.noreply.github.com>
2020-04-28 19:53:07 +0200

    Ass ets

---
+++ _Kacper/Assets/Scripts/MainManu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainManu : MonoBehaviour
+{
+    public Text scoreText;
+
+
+    private void Start()
+    { 
+        scoreText.text = PlayerPrefs.GetInt("score").ToString();
+    }
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game has ended its life. Rest in Pepperoni");
+        Application.Quit();
+    }
+}
